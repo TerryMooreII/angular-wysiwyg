4ee1fafdd2771b8094c0160bc00b67ba22202d09
Kevin Buffington <kevin.buffington@q1media.com>
2015-03-05 14:37:29 -0600

    Adding array notation for proper minification

--- src/angular-wysiwyg.js
+++ src/angular-wysiwyg.js
@@ -44,7 +44,7 @@ Requires:
     ];
 
     angular.module('wysiwyg.module', ['colorpicker.module'])
-        .directive('wysiwyg', function($timeout, wysiwgGui, $compile) {
+        .directive('wysiwyg', ['$timeout', 'wysiwgGui', '$compile', function($timeout, wysiwgGui, $compile) {
             return {
                 template: '<div>' +
                     '<style>' +
@@ -298,7 +298,7 @@ Requires:
                 scope.format('enableobjectresizing', true);
                 scope.format('styleWithCSS', true);
             };
-        })
+        }])
         .factory('wysiwgGui', function(wysiwgGuiElements) {
 
             var ELEMENTS = wysiwgGuiElements;
